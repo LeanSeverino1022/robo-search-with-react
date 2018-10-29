efb53d4ab828e55a8db16e6394c66f1882f74826
Puyih Severino <yihseverino@gmail.com>
2018-10-29 21:52:48 +0800

    add card component

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Robot 1',
+        username: 'one',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Robot 2',
+        username: 'two',
+        email: 'Shanna@melissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Robot 3',
+        username: 'three',
+        email: 'Nathan@yesenia.net'
+    },
+    {
+        id: 4,
+        name: 'Robot 4',
+        username: 'four',
+        email: 'Julianne.OConner@kory.org'
+    },
+    {
+        id: 5,
+        name: 'Robot 5',
+        username: 'five',
+        email: 'Lucio_Hettinger@annie.ca'
+    }
+];
