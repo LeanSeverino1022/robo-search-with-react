9d608ad98211eea37f5e57aff6c9a9e26a7932fb
Puyih Severino <yihseverino@gmail.com>
2018-11-03 00:59:20 +0800

    Create Card component. Add robots data for Card components to use.

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Card.css';
+
+const Card = ({ id, name, email }) => {
+    return (
+        <div className="tc bg-light-green br3 dib pa3 ma2 grow bw2 shadow-5">
+            <img
+                alt={'robot' + id}
+                src={`https://robohash.org/${id}?size=200x200`}
+            />
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    );
+};
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
-import Hello from './Hello';
+import Card from './Card';
+import { robots } from './robots';
 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <Hello greetings="And Hello Ladies" />,
+    <div>
+        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+    </div>,
     document.getElementById('root')
 );
 
