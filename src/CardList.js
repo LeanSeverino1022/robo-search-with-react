024a52e49068ebb9466bef9847eec6195b49f35e
Puyih Severino <yihseverino@gmail.com>
2018-12-13 17:02:31 +0800

    refactor cardsArray rendering

--- src/CardList.js
+++ src/CardList.js
@@ -3,20 +3,21 @@ import Card from './Card';
 
 const CardList = ({ robots }) => {
 
-    
-    const cardsArray = robots.map((robot, i) => {
-        return (
-            <Card
-                key={robot.id}
-                id={robot.id}
-                name={robot.name}
-                email={robot.email}
-            />
-        ); 
-    });
-
     // console.log(Object.prototype.toString.call(cardArray)); //array
-    return <div>{cardsArray}</div>;
+    return (
+        <div>
+            {
+                robots.map((robot, i) => {
+                    return (
+                        <Card key={robot.id}
+                            id={robot.id}
+                            name={robot.name}
+                            email={robot.email} />
+                    )
+                })
+            }
+        </div>
+    );
 };
 
 export default CardList;
